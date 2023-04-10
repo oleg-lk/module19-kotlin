d9cd1adeb0bd28a1e68babd3ab8113db338cd695
Oleg <wismo@yandex.ru>
2023-04-10 22:11:56 +0300

    Перевод на RxJava функции поиска фильмов

--- app/src/main/java/ru/oleshchuk/module19_kotlin/data/TmdbApi.kt
+++ app/src/main/java/ru/oleshchuk/module19_kotlin/data/TmdbApi.kt
@@ -1,5 +1,6 @@
 package ru.oleshchuk.module19_kotlin.data
 
+import io.reactivex.rxjava3.core.Observable
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -17,4 +18,15 @@ interface TmdbApi {
         @Query("page")
         page: Int
     ) : Call<TmdbResultsDTO>
+
+    @GET("search/movie")
+    fun searchFilm(
+        @Query("api_key")
+        api_key: String,
+        @Query("language")
+        language: String,
+        @Query("query")
+        query: String,
+        @Query("page")
+        page: Int): Observable<TmdbResultsDTO>
 }
